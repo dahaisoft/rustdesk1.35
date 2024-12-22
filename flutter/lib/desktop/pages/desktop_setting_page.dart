df633f6c8248f85d2ef7ab917a7ac6b6b144ad84
dahaisoft <57100650+dahaisoft@users.noreply.github.com>
2024-12-22 11:19:38 +0800

    desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1919,7 +1919,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('http://yk.aqdu.cn/privacy.html');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -1927,7 +1927,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('http://yk.aqdu.cn');
                   },
                   child: Text(
                     translate('Website'),
